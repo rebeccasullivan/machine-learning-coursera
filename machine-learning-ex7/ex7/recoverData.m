f6d7278359c59d643638bc80c824c23bd7015bbd
Rebecca <rhhsullivan@gmail.com>
2018-01-27 14:38:53 -0500

    Finish week 8 exercises

--- machine-learning-ex7/ex7/recoverData.m
+++ machine-learning-ex7/ex7/recoverData.m
@@ -18,9 +18,10 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %                    v = Z(i, :)';
 %                    recovered_j = v' * U(j, 1:K)';
 %
-%               Notice that U(j, 1:K) is a row vector.
-%               
+%               Notice that U(j, 1:K) is a row vector.         
 
+U_reduce = U(:, 1:K);
+X_rec = Z * U_reduce';
 
 
 % =============================================================
